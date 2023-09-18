9033d0aecc5d109bc5ccafcd8ffcdb6e1fb3bf6b
AxelHenir <axelhenirmail@gmail.com>
2023-09-17 21:33:42 -0400

    +Added very basic input detection for the spacebar

--- Assets/Scripts/prof.cs
+++ Assets/Scripts/prof.cs
@@ -9,6 +9,15 @@ public class prof : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+
         // if the button is pressed, look at class (change state)
+        if (Input.GetKey("space")){
+            print("THE PROF IS WATCHING");
+            writing = false;
+        } 
+        else {
+            print("THE PROF IS WRITING");
+            writing = true;
+        }
     }
 }
