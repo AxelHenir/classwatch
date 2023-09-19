60330b6cff16d2f1f5404a6af31d64d95a4ae0f2
AxelHenir <axelhenirmail@gmail.com>
2023-09-18 20:50:41 -0400

    Added obedience

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+
+    public bool pause = true;
+    public int roundLength = 30000; // 30 seconds
+
+    public int score = 0;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update(){
+
+    }  
 }
--- Assets/Scripts/student.cs
+++ Assets/Scripts/student.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class student : MonoBehaviour
 {
 
-    //float obedience = 0.0;
+    public float obedience = 0f;
+    float randomNumber;
+
+    // Obedience meter
+    public Image meterBar;
+    public float meterAmount = 100f;
 
     // Update is called once per frame
     void Update(){
-        
+        randomNumber = Random.Range(0, 100);
+        if (randomNumber > 97){
+            Misbehave();
+        }
+
+        if (randomNumber < 3){ // temp 
+            increaseMeter(1); 
+        }
     }
 
     void Misbehave(){
+        reduceMeter(1);
+    }
 
+    public void reduceMeter(float amount){
+        meterAmount -= amount;
+        meterAmount = Mathf.Clamp(meterAmount, 0, 100);
+        meterBar.fillAmount = meterAmount / 100f;
+    }
+
+    public void increaseMeter(float amount){
+        meterAmount += amount;
+        meterAmount = Mathf.Clamp(meterAmount, 0, 100);
+        meterBar.fillAmount = meterAmount / 100f;
     }
 }
