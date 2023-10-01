cf08075e7360393bcaad4f72da0aed24a0c3a988
Azmat Ishaq <azmatishaq@outlook.com>
2023-10-01 12:41:29 -0400

    Updates to guide panel and sound

--- Assets/ShowGuide.cs
+++ Assets/ShowGuide.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class ShowGuide : MonoBehaviour
 {
-
     public GameObject Panel;
 
-    public void OpenPanel() {
-       
+    // This method will be called when the button is clicked to open/close the panel
+    public void OpenPanel()
+    {
         if (Panel != null)
-         {
+        {
             bool isActive = Panel.activeSelf;
-
             Panel.SetActive(!isActive);
         }
-     }
+    }
 
+    // This method will be called when the canvas is clicked to close the panel
+    public void ClosePanel()
+    {
+        if (Panel != null && Panel.activeSelf)
+        {
+            Panel.SetActive(false);
+        }
+    }
 }
+
 
 // Tutorial source for this code:
 // https://www.youtube.com/watch?v=LziIlLB2Kt4
